8ae11134e427ef534b779a8ce9a811d8a6dcd40e
Saba Memon <saba.memon@aalto.fi>
2020-11-05 23:58:36 +0200

    REST API task completed

---
+++ src/main/java/com/task/Employee/EmployeeClasses/EmployeeNotFoundException.java
@@ -0,0 +1,9 @@
+package com.task.Employee.EmployeeClasses;
+
+public class EmployeeNotFoundException extends RuntimeException {
+
+	public EmployeeNotFoundException(String exception) {
+		super(exception);
+	}
+
+}
